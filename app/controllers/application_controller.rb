a462ee5828ff73fae72e6540d56b6471bb6f4a42
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-04-17 15:13:24 +0900

    registrationのformをform_forに書き換え

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :nickname <<:member <<:profile <<:works
+      devise_parameter_sanitizer.for(:sign_up) << :nickname <<:members <<:profile <<:works
     end
 end
