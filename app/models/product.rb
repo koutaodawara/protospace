bb2529b92ece6456be51b1c89a739c46a35a2488
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-04-20 21:12:43 +0900

    get validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
   belongs_to :user
+
+   validates :title, presence: true
+   validates :concept, presence: true
+   validates :catch_copy, presence: true
+   validates :user_id,presence: true
+   # validates :image1, presence: true
+   # validates :image2, presence: true
+   # validates :image3, presence: true
 end
