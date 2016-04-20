faebd37e41da57d3ac318d22fbaae270cfbe02ef
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-04-20 22:42:51 +0900

    modify validate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :user
-
-   validates :title,presence: true
+   validates :title,:concept,:catch_copy ,presence: true
    # validates :image1, presence: true
    # validates :image2, presence: true
    # validates :image3, presence: true
