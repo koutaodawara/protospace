dde22b001c3d88bc7da041945934498546db8f36
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-04-27 17:28:19 +0900

    default image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,24 +1,19 @@
 class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
   storage :file
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # version :avatar do
-  #   process :resize_to_fit => [64, 64]
-  # end
+  version :avatar do
+    process :resize_to_fit => [64, 64]
+  end
 
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
-  def filename
-    time = Time.now
-    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
-    name.downcase
-  end
-
-  # avatar =Magick::ImageList.new("noimage.png")
-  # scaled_img = img.scale(64, 64)
+  avatar = AvatarUploader.new
+  avatar.store!('noimage.png')
 end
 
