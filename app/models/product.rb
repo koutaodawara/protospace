1cc549f8d0a5b17488f5e33c82dace58184ee366
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-04-30 09:05:35 +0900

    definite accepts_nested_attributes_for

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :pictures
+  accepts_nested_attributes_for :pictures
    validates :title,:concept,:catch_copy ,presence: true
    # validates :image1, presence: true
    # validates :image2, presence: true
