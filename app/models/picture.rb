405a288a6309895cb3e5c15635b29f0a038726b2
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-05-03 20:22:48 +0900

    modify model(picture and product)

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,6 @@
 class Picture < ActiveRecord::Base
    enum status: [:main, :sub]
    mount_uploader :image, PictureUploader
-   belongs_to :product
+   belongs_to :product,inverse_of: :pictures
+   validates_presence_of :image
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,14 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :pictures
-  accepts_nested_attributes_for :pictures
-   validates :title,:concept,:catch_copy ,presence: true
+  accepts_nested_attributes_for :pictures,reject_if: :reject_picture
+  validates :title,:concept,:catch_copy,presence: true
+
+    def reject_picture(attributed)
+    attributed['image'].blank?
+    end
+
+    def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 end
