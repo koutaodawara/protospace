49575d0c7e541fdf03d7e43f5325283bf389df8f
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-05-19 22:42:41 +0900

    modify controller to move some action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,29 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+       redirect_to root_path
+    else
+      flash.now[:notice] = "更新に失敗しました"
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.user_id == current_user.id
+      @product.destroy
+      redirect_to root_path
+    end
+  end
+
+
   private
     def product_params
       params.require(:product).permit(:title,:catch_copy,:concept, pictures_attributes: [:image,:status,:product_id])
