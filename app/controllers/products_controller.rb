9ad5b5e6b2d0829b5129dc8cfcea2ea9f32b4f48
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-05-22 15:04:52 +0900

    compile find_method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :product_find, only: [:show,:edit,:update,:destroy]
   def index
     @products = Product.order("created_at DESC")
   end
@@ -19,34 +20,31 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
        redirect_to root_path
     else
-      flash.now[:notice] = "更新に失敗しました"
       render :edit
     end
   end
 
   def destroy
-    @product = Product.find(params[:id])
-    if @product.user_id == current_user.id
       @product.destroy
       redirect_to root_path
-    end
   end
 
 
   private
     def product_params
-      params.require(:product).permit(:title,:catch_copy,:concept, pictures_attributes: [:image,:status,:product_id])
+      params.require(:product).permit(:title,:catch_copy,:concept, pictures_attributes: [:id,:image,:status,:product_id])
+    end
+
+    def product_find
+      @product =Product.find(params[:id])
     end
 end
