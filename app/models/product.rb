6ef70620156386caa90934f907806d7d4cc9c69a
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-05-23 17:55:51 +0900

    add acosiation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :pictures,inverse_of: :product,dependent: :delete_all
+  has_many :likes
   accepts_nested_attributes_for :pictures
   validates :title,:concept,:catch_copy,presence: true
 
