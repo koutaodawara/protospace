8d0a1371cfd1960dc5bd36d602fbb132679d57e9
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-05-30 14:33:00 +0900

    define create destroy in likes_controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,12 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.create(product_id: params[:product_id])
+    @product = Product.find(params[:product_id])
   end
 
   def destroy
     @like = current_user.likes.find_by(product_id: params[:product_id])
+    @product = Product.find(params[:product_id])
     @like.destroy
   end
 end
