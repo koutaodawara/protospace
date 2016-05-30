27552f7229d5eb2de886427bedb01d21eacbaafd
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-05-30 21:46:48 +0900

    add spaces

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ActiveRecord::Base
   belongs_to :user
-  has_many :pictures,inverse_of: :product,dependent: :delete_all
-  has_many :likes,dependent: :delete_all
+  has_many :pictures, inverse_of: :product, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
   accepts_nested_attributes_for :pictures
-  validates :title,:concept,:catch_copy,presence: true
+  validates :title, :concept, :catch_copy, presence: true
 
     def reject_picture(attributed)
     attributed['image'].blank?
