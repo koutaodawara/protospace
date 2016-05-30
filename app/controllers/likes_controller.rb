e8169b57f26b88497ac4736c368118b8e8bb8b10
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-05-30 21:15:22 +0900

    modify like-controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,22 @@
 class LikesController < ApplicationController
+   before_action :product_find, only: [:create, :destroy]
 
   def create
-    @like = current_user.likes.create(product_id: params[:product_id])
-    @product = Product.find(params[:product_id])
+    @like = current_user.likes.create(argument)
   end
 
   def destroy
-    @like = current_user.likes.find_by(product_id: params[:product_id])
-    @product = Product.find(params[:product_id])
+    @like = current_user.likes.find_by(argument)
     @like.destroy
+  end
+
+
+private
+  def argument
+    params.permit(:product_id)
+  end
+
+  def product_find
+     @product = Product.find(params[:product_id])
   end
 end
