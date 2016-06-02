885eceda810657dda897eaa97e66129255ce1a78
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-06-02 18:15:41 +0900

    define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
+  resources :users, only: [:show, :edit, :update]
   resources :products do
-    resources :likes,only: [:create,:destroy]
+    resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create]
   end
 end
