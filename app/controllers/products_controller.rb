d4b44cd7a3173c4ed98949c54efb5e86143dcb9d
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-06-02 21:42:01 +0900

    delete , in product/controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,10 +20,9 @@ class ProductsController < ApplicationController
   end
 
   def show
-      @like = current_user.likes.where(product_id: @product.id ),if user_signed_in?
+      @like = current_user.likes.where(product_id: @product.id ) if user_signed_in?
       @comments = @product.comments
-      @comment = Comment.new
-    end
+      @comment = Comment.new(product_id: @product.id)
   end
 
   def edit
