d6a8e1f63aa22f42969e13cdb036c2a641f64f70
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-06-03 14:55:50 +0900

    add includes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,12 @@
 class CommentsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    @comments = Comment.includes(:product)
     current_user.comments.create(comment_permit)
   end
 
   private
 
   def comment_permit
-    params.require(:comment).permit(:text, :product_id, :user_id)
+    params.require(:comment).permit(:text).merge(product_id: @product.id)
   end
 end
