6bb363b8f82f6e2f65006db5bc798c2995ded11e
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-06-04 19:24:28 +0900

    define "namespace"

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :users, only: [:show, :edit, :update]
+
+   namespace :products do
+    resources :rankings, only: :index
+    resources :newests, only: :index
+  end
+  
   resources :products do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create]
