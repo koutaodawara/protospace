f0a14fac38fd8843e1c635a1c2c91ea69cf22252
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2016-07-10 17:25:26 +0900

    modify rails_helper file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,8 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
+  config.include FactoryGirl::Syntax::Methods
 
   config.filter_rails_from_backtrace!
 end
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
